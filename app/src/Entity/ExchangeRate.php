f3c88894a8acdd0688c30939dd6e2f9690530aa2
Andrew Millar <duckman.andrew@gmail.com>
2020-12-13 21:23:26 +0100

    Add an ExchangRate entity

---
+++ app/src/Entity/ExchangeRate.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ExchangeRateRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ExchangeRateRepository::class)
+ */
+class ExchangeRate
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=3)
+     */
+    private $code;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $rate;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $time;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function setCode(string $code): self
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function getRate(): ?float
+    {
+        return $this->rate;
+    }
+
+    public function setRate(float $rate): self
+    {
+        $this->rate = $rate;
+
+        return $this;
+    }
+
+    public function getTime(): ?int
+    {
+        return $this->time;
+    }
+
+    public function setTime(int $time): self
+    {
+        $this->time = $time;
+
+        return $this;
+    }
+}
